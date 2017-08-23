91a82844d59a3a36aef5142f185ca30dfa80f47f
Jess <jess.leonard@nordstrom.com>
2017-08-23 08:05:18 -0700

    nodejs sample app for spinnaker demo

--- Dockerfile
+++ Dockerfile
@@ -7,7 +7,8 @@ WORKDIR /usr/src/app
 COPY package.json .
 # For npm@5 or later, copy package-lock.json as well
 # COPY package.json package-lock.json .
-
+RUN mkdir /home/nodeJSApp/
+COPY version.txt /home/nodeJSApp/
 RUN npm install
 
 # Bundle app source
--- server.js
+++ server.js
@@ -1,16 +1,32 @@
 'use strict';
 
 const express = require('express');
+const fs = require('fs');
+const http = require('http');
 
 // Constants
-const PORT = 8080;
+const PORT = 8091;
+const HTTPPORT = 8090;
 const HOST = '0.0.0.0';
 
-// App
+var tagline = " -NodeJS Sample App Deployed Through Spinnaker"
+var version = fs.readFileSync('/home/nodeJSApp/version.txt', "utf8");
+
+// App - api
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world\n');
+  res.send('Hello! ' + tagline + '\nVersion: ' + version);
 });
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
+
+//html
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<br>' + '<br>');
+    res.write(" Hello!" + '<br>' + tagline + '<br>' + '<br>');
+    res.write("  Version: " + version + '<br>')
+    res.write(" Date / Time: " + Date());
+    res.end();
+}).listen(`${HTTPPORT}`);
